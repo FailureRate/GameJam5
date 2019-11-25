f6d2126c48d7868d6a42de5137c235790bfa3f52
kidhadez <32069896+kidhadez@users.noreply.github.com>
2019-11-24 19:48:03 -0500

    wave system
    added enemy ai,
    wave spawning,
    wave difficulty increase,
    score display

---
+++ GameJam5/Assets/enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float speedVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedVar = 0.05f;
+        player = FindObjectOfType<RotatePlayer>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        Vector3 velocity = direction.normalized * speedVar * (enemySpawner.waveNumber / 2.0f);
+        transform.position += velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("fireball")) { enemySpawner.scoreNum += 100;enemySpawner.enemyCount--; Destroy(this.gameObject);}
+    }
+}
---
+++ GameJam5/Assets/enemySpawner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemySpawner : MonoBehaviour
+{
+    private float spawnRate;
+    static public int enemyCount;
+    public GameObject enemy;
+    private bool spawnNextWave;
+    private int totalSpawned;
+
+    static public int waveNumber;
+    [SerializeField]
+    private float spawnWait;
+
+    public Text waveText;
+    public Text Score;
+    static public int scoreNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveNumber = 1;
+        totalSpawned = 0;
+        enemyCount = 0;
+        spawnRate = 0.1f;
+        spawnWait = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (spawnWait >= 100)
+        {
+            if (enemyCount < waveNumber * waveNumber && totalSpawned < waveNumber * waveNumber)
+            {
+                spawn();
+                totalSpawned++;
+            }
+        }
+        if (enemyCount == 0 && totalSpawned == waveNumber*waveNumber)
+        {
+            waveNumber++;
+            totalSpawned = 0;
+            enemyCount = 0;
+        }
+        spawnWait += spawnRate * waveNumber;
+        waveText.text = "WAVE: " + waveNumber;
+        Score.text = "SCORE: " + scoreNum;
+    }
+
+    void spawn()
+    {
+        enemy.GetComponent<enemy>().speedVar *= waveNumber/2;
+        Instantiate(enemy, transform.position, Quaternion.Euler(0, 0, 0));
+        enemyCount++;
+        spawnWait = 0;
+    }
+}
